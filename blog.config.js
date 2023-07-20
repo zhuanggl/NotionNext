772b079570d564124a6a45be587200b5fa24f138
zhuanggl <zhuangguolin@gmail.com>
2023-07-20 19:26:25 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '往事并不如风', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '互联网技术人', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.Nobody.me', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.zgl.me', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'architecture, software, cat', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
